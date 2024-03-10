5b751a2ae75c8e5aaf3f5d960aca5b22026d150a
azme12 <azmetefera07@gmail.com>
2024-03-10 08:24:26 +0300

    third commit

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const cors = require('cors');
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://music-app-project-roan.vercel.app'],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
